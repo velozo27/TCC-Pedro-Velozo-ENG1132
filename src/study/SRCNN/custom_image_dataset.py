62bb236e98ab54690cd856d97e30cc77fd36e03c
Pedro Antonio Tibau Velozo <pedrovelozo@Pedros-MacBook-Pro.local>
2023-04-07 18:50:18 -0300

    put CustomImageDataset in separate file

---
+++ src/study/SRCNN/custom_image_dataset.py
@@ -0,0 +1,61 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+from torchvision.io import read_image
+
+
+class CustomImageDataset(Dataset):
+    """
+    A PyTorch dataset for loading a directory of images.
+
+    Args:
+        img_dir (str): The path to the directory containing the images.
+        transform (callable, optional): A function/transform that takes in an image and returns a
+            transformed version. Default: None.
+        target_transform (callable, optional): A function/transform that takes in the target and
+            transforms it. Default: None.
+    """
+    def __init__(self, img_dir, transform=None, target_transform=None):
+        """
+        Initializes a new instance of the CustomImageDataset class.
+
+        Args:
+            img_dir (str): The path to the directory containing the images.
+            transform (callable, optional): A function/transform that takes in an image and returns a
+                transformed version. Default: None.
+            target_transform (callable, optional): A function/transform that takes in the target and
+                transforms it. Default: None.
+        """
+        self.file_lst = list(os.listdir(img_dir))
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        """
+        Returns the number of images in the dataset.
+
+        Returns:
+            The number of images in the dataset.
+        """
+        return len(self.file_lst)
+
+    def __getitem__(self, idx):
+        """
+        Returns the image at the specified index in the dataset.
+
+        Args:
+            idx (int): The index of the image to return.
+
+        Returns:
+            A tuple containing the transformed input image and the transformed target image.
+        """
+        img_path = self.file_lst[idx]
+        image = read_image(f"{self.img_dir}/{img_path}")
+        image_x = image.detach().clone()
+        image_y = image.detach().clone()
+        if self.transform:
+            image_x = self.transform(image_x)
+        if self.target_transform:
+            image_y = self.target_transform(image_y)
+        return image_x, image_y
