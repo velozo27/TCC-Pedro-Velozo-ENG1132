9a6b391bf0d1bd1f39f61fc17415847658350de4
Pedro Antonio Tibau Velozo <pedrovelozo@Pedros-MacBook-Pro.local>
2023-04-07 18:33:58 -0300

    make image_helper adjustments

--- src/study/SRCNN/image_helper.py
+++ src/study/SRCNN/image_helper.py
@@ -1,15 +1,29 @@
-import matplotlib.pyplot as plt
 import torch
+import matplotlib.pyplot as plt
 
+class ImageHelper:
+    """
+    A helper class for working with image data in PyTorch.
+    """
 
-class ImageHelper():
-    def __init__(self) -> None:
+    def __init__(self):
+        """
+        Initializes a new instance of the ImageHelper class.
+        """
         return
 
-    def show_tensor_as_image(tensor: torch.Tensor) -> None:
-        plt.figure()
+    def show_tensor_as_image(self, tensor: torch.Tensor):
+        """
+        Displays an image represented as a PyTorch tensor.
 
+        Args:
+            tensor: A PyTorch tensor representing the image. The tensor should have shape (C, H, W),
+                where C is the number of channels, H is the height, and W is the width.
+
+        Returns:
+            None
+        """
+        plt.figure()
         tensor_np = tensor.numpy()
         plt.imshow(tensor_np.transpose((1, 2, 0)))
-
         plt.show()
